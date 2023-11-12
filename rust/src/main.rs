97cba8cf11d0dafa44ff4f88f7b59fce88b3ccfc
Vallsp <99552376+Vallsp@users.noreply.github.com>
2023-11-12 17:43:19 +0000

    api Rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,45 +1,58 @@
-use std::net::{TcpStream, TcpListener};
-use std::io::{Read, Write};
+use std::env;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 use std::thread;
 
+fn handle_client(mut stream: TcpStream) {
+    let mut headers = Vec::new();
 
-fn handle_read(mut stream: &TcpStream) {
-    let mut buf = [0u8 ;4096];
-    match stream.read(&mut buf) {
-        Ok(_) => {
-            let req_str = String::from_utf8_lossy(&buf);
-            println!("{}", req_str);
-            },
-        Err(e) => println!("Unable to read stream: {}", e),
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let request = String::from_utf8_lossy(&buffer[..]);
+
+    for line in request.lines() {
+        if line.is_empty() {
+            break;
+        }
+        headers.push(line.to_string());
     }
-}
 
-fn handle_write(mut stream: TcpStream) {
-    let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello world</body></html>\r\n";
-    match stream.write(response) {
-        Ok(_) => println!("Response sent"),
-        Err(e) => println!("Failed sending response: {}", e),
+    let request_line = headers[0].split_whitespace().collect::<Vec<&str>>();
+    if request_line.len() != 3 {
+        return;
     }
-}
+    let method = request_line[0];
+    let path = request_line[1];
 
-fn handle_client(stream: TcpStream) {
-    handle_read(&stream);
-    handle_write(stream);
+    if method == "GET" && path == "/ping" {
+        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n{:?}", headers);
+        stream.write(response.as_bytes()).unwrap();
+    } else {
+        let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        stream.write(response.as_bytes()).unwrap();
+    }
+
+    stream.flush().unwrap();
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    println!("Listening for connections on port {}", 8080);
+    let port = env::var("PING_LISTEN_PORT").unwrap_or("8080".to_string());
+    let addr = format!("127.0.0.1:{}", port);
+
+    let listener = TcpListener::bind(&addr).unwrap();
+
+    println!("Listening on port {}...", port);
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 thread::spawn(|| {
-                    handle_client(stream)
+                    handle_client(stream);
                 });
             }
             Err(e) => {
-                println!("Unable to connect: {}", e);
+                println!("Error: {}", e);
             }
         }
     }
